2cb52cb76b0ee4569e1255c726dded4733a6f229
dhananjay-at-885838946818 <dhananjay@faithconsultancy.in>
2019-09-13 17:35:16 +0530

    popup Notifier code

---
+++ PopupNotification/PopupNotification/Popup.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Tulpep.NotificationWindow;
+
+namespace PopupNotification
+{
+    public partial class Popup : Form
+    {
+        public Popup()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            PopupNotifier popup = new PopupNotifier();
+            popup.TitleText = "Testing";
+            popup.ContentText = "This is test by dhananjay";
+            popup.Popup();
+        }
+    }
+}
